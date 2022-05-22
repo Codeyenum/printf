80b692e1ba1bb952bc551ed831217974bcbf3a98
Nnadi Charis <charisnnadi15@gmail.com>
2022-05-22 16:40:25 -0700

    str

--- main.h
+++ main.h
@@ -22,5 +22,6 @@ typedef struct print
 int _printf(const char *format, ...);
 int print_prg(va_list __attribute__((unused)), char *, unsigned int);
 int print_chr(va_list arguments, char *buf, unsigned int ibuf);
+int print_str(va_list arguments, char *buf, unsigned int ibuf);
 
 #endif
