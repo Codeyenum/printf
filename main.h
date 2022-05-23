f2293928c63a41afc23ab771ffbaecce5f6b99e7
Nnadi Charis <charisnnadi15@gmail.com>
2022-05-22 19:10:08 -0700

    binary array

--- main.h
+++ main.h
@@ -50,6 +50,7 @@ int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 int ev_print_func(const char *s, int index);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
 int print_buf(char *buf, unsigned int nbuf);
+char *fill_binary_array(char *binary, long int int_in, int isneg, int limit);
 
 
 #endif
