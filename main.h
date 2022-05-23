e91190976244e9a5a0610874037f10fb5b9b8ac6
Nnadi Charis <charisnnadi15@gmail.com>
2022-05-22 19:06:31 -0700

    buffer print

--- main.h
+++ main.h
@@ -49,6 +49,7 @@ int prinnupx(va_list arguments, char *buf, unsigned int ibuf);
 int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 int ev_print_func(const char *s, int index);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
+int print_buf(char *buf, unsigned int nbuf);
 
 
 #endif
