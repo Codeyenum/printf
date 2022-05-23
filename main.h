667e7b58c54ab503f7c3083babc3f1f0a73244b3
Nnadi Charis <charisnnadi15@gmail.com>
2022-05-22 18:58:52 -0700

    ev function

--- main.h
+++ main.h
@@ -47,6 +47,7 @@ int prinnoct(va_list arguments, char *buf, unsigned int ibuf);
 int prinnhex(va_list arguments, char *buf, unsigned int ibuf);
 int prinnupx(va_list arguments, char *buf, unsigned int ibuf);
 int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
+int ev_print_func(const char *s, int index);
 
 
 #endif
