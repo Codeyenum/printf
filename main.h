f97eefc8c0d685f7aaa8f3d8f4aacef26e9369b4
Nnadi Charis <charisnnadi15@gmail.com>
2022-05-22 19:03:15 -0700

    buffer h

--- main.h
+++ main.h
@@ -48,6 +48,7 @@ int prinnhex(va_list arguments, char *buf, unsigned int ibuf);
 int prinnupx(va_list arguments, char *buf, unsigned int ibuf);
 int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 int ev_print_func(const char *s, int index);
+unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
 
 
 #endif
