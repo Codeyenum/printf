733e1726361b90802df8cada415b625beec092e8
Nnadi Charis <charisnnadi15@gmail.com>
2022-05-22 17:51:09 -0700

    long hex

--- main.h
+++ main.h
@@ -34,6 +34,7 @@ int print_rev(va_list arguments, char *buf, unsigned int ibuf);
 int print_rot(va_list arguments, char *buf, unsigned int ibuf);
 int prinlint(va_list arguments, char *buf, unsigned int ibuf);
 int prinloct(va_list arguments, char *buf, unsigned int ibuf);
+int prinlhex(va_list arguments, char *buf, unsigned int ibuf);
 
 
 #endif
