07a9f6a6be7bdc7b2e85c7969ed1dcfbdbbd2cf4
Nnadi Charis <charisnnadi15@gmail.com>
2022-05-22 17:58:15 -0700

    long upx

--- main.h
+++ main.h
@@ -35,6 +35,7 @@ int print_rot(va_list arguments, char *buf, unsigned int ibuf);
 int prinlint(va_list arguments, char *buf, unsigned int ibuf);
 int prinloct(va_list arguments, char *buf, unsigned int ibuf);
 int prinlhex(va_list arguments, char *buf, unsigned int ibuf);
+int prinlupx(va_list arguments, char *buf, unsigned int ibuf);
 
 
 #endif
