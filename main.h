484cef5575d0d22b9620d9b7d6a82d615da3d03c
Nnadi Charis <charisnnadi15@gmail.com>
2022-05-22 18:16:22 -0700

    short hex

--- main.h
+++ main.h
@@ -39,6 +39,7 @@ int prinlupx(va_list arguments, char *buf, unsigned int ibuf);
 int prinhint(va_list arguments, char *buf, unsigned int ibuf);
 int prinhunt(va_list arguments, char *buf, unsigned int ibuf);
 int prinhoct(va_list arguments, char *buf, unsigned int ibuf);
+int prinhhex(va_list arguments, char *buf, unsigned int ibuf);
 
 
 #endif
