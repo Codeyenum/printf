21361279a3e43e2975fc21419fd7153502f30454
Nnadi Charis <charisnnadi15@gmail.com>
2022-05-22 17:04:47 -0700

    octal

--- main.h
+++ main.h
@@ -26,5 +26,6 @@ int print_str(va_list arguments, char *buf, unsigned int ibuf);
 int print_int(va_list arguments, char *buf, unsigned int ibuf);
 int print_bnr(va_list arguments, char *buf, unsigned int ibuf);
 int print_unt(va_list arguments, char *buf, unsigned int ibuf);
+int print_oct(va_list arguments, char *buf, unsigned int ibuf);
 
 #endif
