1a4d9e36ec77ed925231d2ba0de169755b6c36ba
Nnadi Charis <charisnnadi15@gmail.com>
2022-05-22 17:37:50 -0700

    rot13

--- main.h
+++ main.h
@@ -31,6 +31,7 @@ int print_hex(va_list arguments, char *buf, unsigned int ibuf);
 int print_upx(va_list arguments, char *buf, unsigned int ibuf);
 int print_add(va_list arguments, char *buf, unsigned int ibuf);
 int print_rev(va_list arguments, char *buf, unsigned int ibuf);
+int print_rot(va_list arguments, char *buf, unsigned int ibuf);
 
 
 #endif
