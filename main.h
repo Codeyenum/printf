b378fd787f4f2debf9b5f254b12415346b726eab
Nnadi Charis <charisnnadi15@gmail.com>
2022-05-22 18:48:52 -0700

    conv upx

--- main.h
+++ main.h
@@ -45,6 +45,7 @@ int prinsint(va_list arguments, char *buf, unsigned int ibuf);
 int prinpint(va_list arguments, char *buf, unsigned int ibuf);
 int prinnoct(va_list arguments, char *buf, unsigned int ibuf);
 int prinnhex(va_list arguments, char *buf, unsigned int ibuf);
+int prinnupx(va_list arguments, char *buf, unsigned int ibuf);
 
 
 #endif
